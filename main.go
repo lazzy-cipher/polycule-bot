ae4f5c5c83ddb93d8021fb5c8865089b70feca62
Lazzy L. Cipher <lazzy.cipher@proton.me>
2026-07-31 14:22:29 -0400

    Added more replies

--- main.go
+++ main.go
@@ -146,6 +146,8 @@ var (
 		"<@" + AdminID + "> programmed me too many feelings",
 		"im too poly for that kind of stuff",
 		"if i could smell like something id like to smell like bubble cum",
+		"*cute autistic stimming",
+		"*gets constipated from joy* AAA why does it always do that!!",
 	}
 	ReplyChannelBlacklist = [...]string{
 		"1532407644919431218", // memories
